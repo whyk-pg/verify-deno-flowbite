ba41056798c88f2f9d5c2de2d44161f237f7b1aa
WhyK <windchimeyk@gmail.com>
2025-04-12 13:11:30 +0900

    style: deno fmt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button type="button" onClick={() => setCount((count: number) => count + 1)}>
+        <button
+          type="button"
+          onClick={() => setCount((count: number) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
